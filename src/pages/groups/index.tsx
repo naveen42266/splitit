b754a3878ed48e51cd5024e48684bbc2996a6286
Naveen <vnaveenlgp2001@gmail.com>
2024-07-18 16:52:49 +0530

    group screen

--- src/pages/groups/index.tsx
+++ src/pages/groups/index.tsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar";
 import AddIcon from '@mui/icons-material/Add';
 import Avatar from "@mui/material/Avatar";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CreateGroup from "../../components/createGroup";
 const Groups = () => {
-    const groups = [{ name: 'kodaikanal dairy' }]
+    const groups = [{ id: '1', name: 'kodaikanal dairy' }]
     const [createGroup, setCreateGroup] = useState(false)
     const [width, setWidth] = useState(0);
+    const [group, setGroup] = useState('')
 
     function handleList(newGroup: boolean, group: number) {
         if (newGroup) {
             return <CreateGroup handleBack={(back: boolean) => { setCreateGroup(back) }} />
         }
-        return <div className="w-full">
+        return <div>
             {groupsList()}
         </div>
     }
@@ -21,11 +23,11 @@ const Groups = () => {
         return <div>
             <div className={`flex justify-between items-center mb-3 ${width < 768 ? 'mx-4 mt-4' : ''}`}>
                 <div className="text-lg">Groups</div>
-                <AddIcon onClick={() => { setCreateGroup(!createGroup) }} />
+                <AddIcon className="mx-4" onClick={() => { setCreateGroup(!createGroup) }} />
             </div>
             {groups?.map((each, index) => {
                 return (
-                    <div key={index} className={`flex items-center cursor-pointer pl-4 bg-white hover:bg-[#f5f6f6] `} onMouseEnter={() => { }} onMouseLeave={() => { }} onClick={() => { }}>
+                    <div key={index} className={`flex items-center cursor-pointer pl-4 bg-white hover:bg-[#f5f6f6] `} onMouseEnter={() => { }} onMouseLeave={() => { }} onClick={() => { setGroup(each?.id) }}>
                         <div className="">
                             <Avatar src={''} alt="Naveen" sx={{ width: 50, height: 50 }} />
                         </div>
@@ -48,6 +50,34 @@ const Groups = () => {
         </div>
     }
 
+    function handleDetailScreen() {
+        if (((width >= 768) || (groups?.length == 0 && width < 768 && !createGroup)) && group == '') {
+            return <div className={`flex flex-col justify-between items-center h-[300px] w-full my-20 md:my-30 `}>
+                <div className="text-lg">{groups?.length == 0 ? 'No Groups available' : 'Choose Group'}</div>
+                <img src="https://img.freepik.com/free-vector/smiling-person-crowd_23-2148422588.jpg" alt="" />
+                <div className="block md:hidden px-7 text-lg bg-blue-500 text-white rounded-md" onClick={() => { setCreateGroup(!createGroup) }}>Create Group</div>
+            </div>
+        }
+        else if (group != '') {
+            return <div className="">
+                <div className="flex items-center gap-3 px-3 py-2 bg-[#f0f2f5]" onClick={() => { }}>
+                    <ArrowBackIcon className="block md:hidden" onClick={()=>{setGroup('')}}/>
+                    <Avatar alt="Naveen" src={''} sx={{ width: "42px", height: "42px" }} />
+                    <div>
+                        <div>{groups?.find((ele) => ele?.id == group)?.name}</div>
+                        <div className="text-sm text-[#667781]">4 members</div>
+                    </div>
+                </div>
+            </div>
+        }
+        else if (width <= 768 && groups?.length >= 1 && !createGroup){
+            return  <div className={``}>{groupsList()}</div>
+        }
+
+        // 1st if condition ${((width >= 768) || (groups?.length == 0 && width < 768 && !createGroup)) &&  group == '' ? 'block' : 'hidden'}`}
+        // 3rd condition else if ${width <= 768 && groups?.length >= 1 && !createGroup ? 'block' : 'hidden'}
+    }
+
     useEffect(() => {
         if (typeof window !== 'undefined') {
             const updateWidth = () => setWidth(window.innerWidth);
@@ -59,18 +89,12 @@ const Groups = () => {
     return (
         <div>
             <Navbar />
-            <div className="flex">
+            <div className="flex w-full relative overflow-y-hidden">
                 <div className={`${createGroup || groups?.length >= 1 ? 'w-[30%] hidden md:block mt-10 ml-8' : 'hidden'}`}>{handleList(createGroup, groups?.length)}</div>
                 <div className={`${(createGroup || groups?.length >= 1) && width >= 768 ? 'w-[70%]' : 'w-[100%] block'}`}>
                     {!createGroup && groups?.length == 0 && <div className="flex justify-end"><AddIcon className="hidden md:block bg-blue-500 text-white rounded-md mt-10 mr-10" fontSize="large" onClick={() => { setCreateGroup(!createGroup) }} /></div>}
                     {createGroup && <div className="block md:hidden mr-5 mt-5 ml-5"><CreateGroup handleBack={(back: boolean) => { setCreateGroup(back) }} /></div>}
-                    {/*  ${width <= 768 && createGroup && groups?.length == 0 ? 'hidden' : 'block'}    width < 768 && groups?.length !== 0*/}
-                    <div className={`flex flex-col justify-between items-center h-[300px] w-full my-20 md:my-30 ${ ( width >= 768) || (groups?.length == 0 && width < 768 && !createGroup) ? 'block' : 'hidden'}`}>
-                        <div className="text-lg">{groups?.length == 0 ? 'No Groups available' : 'Choose Group'}</div>
-                        <img src="https://img.freepik.com/free-vector/smiling-person-crowd_23-2148422588.jpg" alt="" />
-                        <div className="block md:hidden px-7 text-lg bg-blue-500 text-white rounded-md" onClick={() => { setCreateGroup(!createGroup) }}>Create Group</div>
-                    </div>
-                    <div className={`${width <= 768 && groups?.length >= 1 && !createGroup ? 'block' : 'hidden'}`}>{groupsList()}</div>
+                    {handleDetailScreen()}
                 </div>
             </div>
         </div>
