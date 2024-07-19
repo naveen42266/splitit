cc8036490e05a091bc7ff14262ab061248492787
Naveen <vnaveenlgp2001@gmail.com>
2024-07-19 18:16:36 +0530

    group screen

--- src/pages/groups/index.tsx
+++ src/pages/groups/index.tsx
@@ -3,12 +3,71 @@ import Navbar from "../../components/navbar";
 import AddIcon from '@mui/icons-material/Add';
 import Avatar from "@mui/material/Avatar";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Tab, Tabs } from "@mui/material";
 import CreateGroup from "../../components/createGroup";
+const list = [
+    {
+        name: 'sanjay',
+        month: 'jul',
+        date: '20',
+        for: 'break fast',
+        paidAmt: '500'
+    }, {
+        name: 'saran',
+        month: 'jul',
+        date: '20',
+        for: 'lunch',
+        paidAmt: '1500'
+    },
+    {
+        name: 'sanjay',
+        month: 'jul',
+        date: '19',
+        for: 'room rent',
+        paidAmt: '5500'
+    }, {
+        name: 'saran',
+        month: 'jul',
+        date: '19',
+        for: 'W***y',
+        paidAmt: '3500'
+    },
+    {
+        name: 'naveen',
+        month: 'jul',
+        date: '18',
+        for: 'chicken',
+        paidAmt: '400'
+    },
+    {
+        name: 'gowtham',
+        month: 'jul',
+        date: '18',
+        for: 'dinner',
+        paidAmt: '400'
+    },
+    {
+        name: 'gowtham',
+        month: 'jul',
+        date: '17',
+        for: 'snacks',
+        paidAmt: '400'
+    },
+    {
+        name: 'naveen',
+        month: 'jul',
+        date: '17',
+        for: 'juice',
+        paidAmt: '400'
+    }
+]
 const Groups = () => {
     const groups = [{ id: '1', name: 'kodaikanal dairy' }]
+    const tabs = ['Settle Up', 'Balance', 'Total', 'Convert to USD',]
     const [createGroup, setCreateGroup] = useState(false)
     const [width, setWidth] = useState(0);
     const [group, setGroup] = useState('')
+    const [tab, setTab] = useState<any>('Settle Up');
 
     function handleList(newGroup: boolean, group: number) {
         if (newGroup) {
@@ -59,21 +118,82 @@ const Groups = () => {
             </div>
         }
         else if (group != '') {
-            return <div className="">
-                <div className="flex items-center gap-3 px-3 py-2 bg-[#f0f2f5]" onClick={() => { }}>
-                    <ArrowBackIcon className="block md:hidden" onClick={()=>{setGroup('')}}/>
-                    <Avatar alt="Naveen" src={''} sx={{ width: "42px", height: "42px" }} />
-                    <div>
-                        <div>{groups?.find((ele) => ele?.id == group)?.name}</div>
-                        <div className="text-sm text-[#667781]">4 members</div>
+            return <div className="relative h-[88%] w-full">
+                <div className="sticky top-0 left-0 right-0">
+                    <div className="flex items-center gap-3 px-3 py-2 bg-[#f0f2f5]" onClick={() => { }}>
+                        <ArrowBackIcon className="block md:hidden" onClick={() => setGroup('')} />
+                        <Avatar alt="Naveen" src={''} sx={{ width: "42px", height: "42px" }} />
+                        <div>
+                            <div>{groups?.find((ele) => ele?.id == group)?.name}</div>
+                            <div className="text-sm text-[#667781]">4 members</div>
+                        </div>
                     </div>
+                    <Tabs
+                        className="border-b border-slate-300"
+                        value={tab}
+                        onChange={(event: React.SyntheticEvent, newValue: any) => setTab(newValue)}
+                        aria-label="basic tabs example"
+                        sx={{ minWidth: '50px' }}
+                    >
+                        {tabs?.map((ele, index) => (
+                            <Tab key={index} label={ele} value={ele} sx={{ minWidth: '45px' }} />
+                        ))}
+                    </Tabs>
+                </div>
+                <div className="h-[91%] overflow-y-scroll">
+                    {handleTabContent()}
                 </div>
             </div>
-        }
-        else if (width <= 768 && groups?.length >= 1 && !createGroup){
-            return  <div className={``}>{groupsList()}</div>
-        }
 
+        }
+        else if (width <= 768 && groups?.length >= 1 && !createGroup) {
+            return <div className={``}>{groupsList()}</div>
+        }
+        function handleTabContent() {
+            if (tab == 'Settle Up') {
+                return <div className="my-4">
+                    <div className="text-sm font-semibold mx-4">July 2024</div>
+                    <div className="">
+                        {list?.map((ele, index) => {
+                            return (
+                                <div key={index} className={`hover:bg-[#f0f2f5] ${index == 0 ?? 'mt-2'} `}>
+                                    <div className="flex justify-start py-3 mx-4">
+                                        <div className="w-[10%] text-slate-500">
+                                            <div>{ele?.month}</div>
+                                            <div>{ele?.date}</div>
+                                        </div>
+                                        <div className="flex justify-between w-[90%]">
+                                            <div>
+                                                <div className="text-blue-500 font-semibold text-base">{ele?.for}</div>
+                                                <div className="">{ele?.name} paid {ele?.paidAmt}</div>
+                                            </div>
+                                            <div>
+                                                <div className="text-blue-500 cursor-pointer">Edit</div>
+                                                <div className="text-red-500 cursor-pointer">Delete</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-[10%]"></div>
+                                        <div className="w-[90%] border-b border-slate-200 ml-4"></div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        <div className="h-20"></div>
+                    </div>
+                </div>
+            }
+            else if (tab == 'Balance') {
+                return <div className="m-4">Balance</div>
+            }
+            else if (tab == 'Total') {
+                return <div className="m-4">Total</div>
+            }
+            else if (tab == 'Convert to USD') {
+                return <div className="m-4">Convert to USD</div>
+            }
+        }
         // 1st if condition ${((width >= 768) || (groups?.length == 0 && width < 768 && !createGroup)) &&  group == '' ? 'block' : 'hidden'}`}
         // 3rd condition else if ${width <= 768 && groups?.length >= 1 && !createGroup ? 'block' : 'hidden'}
     }
@@ -87,9 +207,9 @@ const Groups = () => {
         }
     }, []);
     return (
-        <div>
+        <div className="h-screen w-full overflow-y-hidden">
             <Navbar />
-            <div className="flex w-full relative overflow-y-hidden">
+            <div className="flex relative">
                 <div className={`${createGroup || groups?.length >= 1 ? 'w-[30%] hidden md:block mt-10 ml-8' : 'hidden'}`}>{handleList(createGroup, groups?.length)}</div>
                 <div className={`${(createGroup || groups?.length >= 1) && width >= 768 ? 'w-[70%]' : 'w-[100%] block'}`}>
                     {!createGroup && groups?.length == 0 && <div className="flex justify-end"><AddIcon className="hidden md:block bg-blue-500 text-white rounded-md mt-10 mr-10" fontSize="large" onClick={() => { setCreateGroup(!createGroup) }} /></div>}
